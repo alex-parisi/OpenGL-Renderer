649d7e2f3bb5d4da4f398e9c9ad4e7839522622e
Alex Parisi <alex.parisi@outlook.com>
2023-08-26 00:57:14 -0400

    Added functionality to duplicate an object
    Also added functionality to apply different transformations to each instance of an object

--- OpenGL-Renderer/OpenGL-Renderer/shader.cpp
+++ OpenGL-Renderer/OpenGL-Renderer/shader.cpp
@@ -58,24 +58,48 @@ void Shader::Draw()
     // Activate shader:
     glUseProgram(shaderProgram);
     // Create transformations
-    float t = (float)glfwGetTime();
-    glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
-    model = glm::rotate(model, t, glm::vec3(0.5f, 1.0f, 0.0f));
-    model = glm::scale(model, glm::vec3(0.125 * sin(4 * t) + 0.875, 0.125 * sin(4 * t) + 0.875, 0.125 * sin(4 * t) + 0.875));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
     projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     // Get matrix uniform locations
     unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
     // Pass them to the shaders
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     setMat4("projection", projection);
+    setMat4("view", view);
     // Render container
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+    int idx = 0;
+    for (glm::vec3 position : cubePositions)
+    {
+        float t = (float)glfwGetTime();
+        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        model = glm::translate(model, position);
+        float angle = 20.0f * idx;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        if (idx % 3 == 0)
+        {
+            model = glm::rotate(model, t, glm::vec3(0.5f, 1.0f, 0.0f));
+            model = glm::scale(model, glm::vec3(0.125 * sin(4 * t) + 0.875, 0.125 * sin(4 * t) + 0.875, 0.125 * sin(4 * t) + 0.875));
+        }
+        idx++;
+        setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+    
 }
 
 void Shader::EnableWireframeMode()
